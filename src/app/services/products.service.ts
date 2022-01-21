340eb54cb2c95534b1d81a986057c23eae704a19
David Amador <54488581+dsamador@users.noreply.github.com>
2022-01-20 21:09:49 -0500

    reintentando petición con retry

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-
+import { retry, retryWhen } from 'rxjs/operators';
 import { Product, CreateProductDTO } from './../models/product.model';
 
 @Injectable({
@@ -20,7 +20,10 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(this.apiUrl, {params});
+    return this.http.get<Product[]>(this.apiUrl, {params})
+    .pipe(
+      retry(3)
+    );
   }
 
   getProduct(id: string){
